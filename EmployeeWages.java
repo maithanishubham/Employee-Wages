92cc3c36b088e6206c51e1139b3afebdd3f2a479
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 10:11:09 -0700

    calculate wages for month

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -5,31 +5,33 @@ public class EmployeeWages {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Attendance");
-		 int dailyWage = 0;
-		 int noofpartHourperday = 4;
-		 int noOfhourperday = 8;
-		 int Wageperhr = 20;
-		     int attendence= (int) (Math.floor(Math.random()*10))%2;
-		     
-		switch (attendence) {
-
-		case 1:
-			System.out.println("Employee is Working Full Time");
-			dailyWage= Wageperhr * noOfhourperday;
-			System.out.println(" Employee daily wage is :" + dailyWage );
-		  break;
-		case 0:
-		    System.out.println("Employee is working part time");
-		    dailyWage= Wageperhr * noofpartHourperday ;
-		    System.out.println("Employee daily wage is: "+ dailyWage);
-		    break;
-
-		 default :
-		 System.out.println("Employee is absent");
-		    break;
-		}
+		 int Part_time = 1;
+		   int  Full_time = 2;
+		   int WAGE_PER_HR = 20;
+		   int WORKING_DAYS = 20;
+		   int totalWage = 0;
+		   
+		   for (int i = 1; i <= WORKING_DAYS; i++)
+		   {
+		       int empType = (int) (Math.random() * 10 ) % 2;
+		       int workingHours =0;
+		       switch (empType)
+		       {
+		           case 1:
+		               workingHours = 4;
+		               break;
+		           case 2:
+		               workingHours = 8;
+		               break;
+		           default:
+		       }
+		       
+		       int wage = workingHours * WAGE_PER_HR;
+		       System.out.println("Day " + i + " wage is:" + wage);
+		       totalWage += wage;
+		   }
+		   System.out.println("Total wage for a month is " + totalWage);        
 	}
 }
-
 		
 		
