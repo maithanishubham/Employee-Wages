f6514d8553fc46f602268f5741f9ca8ac8c37c2c
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 09:44:09 -0700

    employee present or absent

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -4,7 +4,16 @@ public class EmployeeWages {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Attendence");
+		
+		 int Attendance=1; 
+		  double randomCheck= (Math.floor(Math.random()*10))%2;
+		  if (Attendance==randomCheck) {
+			  System.out.println("Employee is present");
+			  
+		  } else {
+			  System.out.println("Employee is absent");
+		  }
 
-	}
+			}
 
 }
