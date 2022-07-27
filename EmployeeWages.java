c5c26a29a19d77e974e4c0b5dd368ac047e45741
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 10:02:55 -0700

    daily employee wages

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -3,26 +3,31 @@ package com.employee;
 public class EmployeeWages {
 
 	public static void main(String[] args) {
-		System.out.println ("Welcome to Employee Attendance");
+		System.out.println("Welcome to Employee Attendance");
+		 int dailyWage = 0;
+		 int noofpartHourperday = 4;
 		 int noOfhourperday = 8;
 		 int Wageperhr = 20;
-		 
-		  int Attendance=1; 
-		  double randomCheck= (Math.floor(Math.random()*10)) %2;
-		  
-		  if (Attendance == randomCheck) {
-			  
-			  if(noOfhourperday == 8) {
-			  System.out.println("Employee is present");
-			  int totalWage = noOfhourperday * Wageperhr;
-			  System.out.println("wage per day is:" + totalWage);
-			  
-			  }  
-		  } 
-			  
-			   else {
-			  System.out.println(" Employee is Absent");
-		  }
+		     int attendence= (int) (Math.floor(Math.random()*10))%2;
+		     
+		switch (attendence) {
 
-			}
+		case 1:
+			System.out.println("Employee is Working Full Time");
+			dailyWage= Wageperhr * noOfhourperday;
+			System.out.println(" Employee daily wage is :" + dailyWage );
+		  break;
+		case 0:
+		    System.out.println("Employee is working part time");
+		    dailyWage= Wageperhr * noofpartHourperday ;
+		    System.out.println("Employee daily wage is: "+ dailyWage);
+		    break;
+
+		 default :
+		 System.out.println("Employee is absent");
+		    break;
 		}
+
+
+		}
+}
