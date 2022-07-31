88605f7c1a256c0f98a1ab36a5ac8683dca8588a
shubham maithani <shubhammaithani1@gmail.com>
2022-07-31 11:09:54 -0700

    Wages for multiple companies

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -4,29 +4,61 @@ public class EmployeeWages {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Employee Attendance");
+		System.out.println("Welcome to the Employee Wage Computation program");
 		
-		EmployeeWages Wage= new EmployeeWages();
-		Wage.Employee();
+		
+		EmployeeWages  bigBazar=new EmployeeWages("BigBazar",20,2,10);
+		System.out.println("Total Employee Wage for Company "+ bigBazar.company+" is: "+bigBazar.computeEmpWage());
+		
+	     EmployeeWages tata=new EmployeeWages("tata",10,2,20);
+		System.out.println("Total Employee Wage for Company "+ tata.company+" is: "+tata.computeEmpWage());
 		}
-		public void Employee() {
-	          int noOfhourperday = 8;
-	          int Wageperhr = 20;
-	          int Attendance=1; 
-	  double randomCheck= (Math.floor(Math.random()*10)) %2;
-	  if (Attendance == randomCheck) {
-		  
-		  if(noOfhourperday == 8) {
-		  System.out.println("Employee is present");
-		  int totalWage = noOfhourperday * Wageperhr;
-		  System.out.println("wage per day is:" + totalWage);
-		  
-		  }  
-	  } 
-		  
-		   else {
-		  System.out.println(" Employee is Absent");
-	  }   
-	}	
+
+		
+	    private static final int IS_PART_TIME=1; 
+		private static final int IS_FULL_TIME=2; 
+		
+		private final String company;
+		private final int empRatePerHour; 
+		private final int maxHoursPerMonth; 
+		private final int numOfWorkingDays;
+		
+		
+		public EmployeeWages(String company, int empRatePerHour, int maxHoursPerMonth, int numOfWorkingDays)
+	        {
+		
+			this.company = company;
+			this.empRatePerHour = empRatePerHour;
+			this.maxHoursPerMonth = maxHoursPerMonth;
+			this.numOfWorkingDays = numOfWorkingDays;
+		}
+
+	         private int computeEmpWage() 
+	         	
+	          {
+			int empHrs=0, totalEmpHrs=0,totalWorkingDays=0;
+			
+
+			while(totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+	                {
+				 totalWorkingDays++; 
+				 int attendance=(int)(Math.random()*3); 
+				 switch(attendance) 
+				 { 
+				 case IS_FULL_TIME:empHrs=8;
+				 break; 
+				 case IS_PART_TIME:empHrs=4;
+				 break;
+				 default:empHrs=0; 
+				 break; 
+				 } 
+				 totalEmpHrs=(totalEmpHrs+empHrs); 
+				 System.out.println("Days= "+ totalWorkingDays+ " Emp Hr: "+empHrs);
+			}
 				
-}
+				 
+			return totalEmpHrs*empRatePerHour;
+			
+		}
+		
+	}
