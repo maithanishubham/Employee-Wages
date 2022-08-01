62e16ac58bca4f0f52bd7db9310b60b7574c576f
shubham maithani <shubhammaithani1@gmail.com>
2022-08-01 10:00:04 -0700

    EmpWage Builder to get total wage

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,5 +1,7 @@
 package com.employee;
 import java.util.ArrayList;
+import java.util.Iterator;
+
 
 interface ComputeEmpWage{
 	public void addCompanyEmpWage(String company, int wagePerhour, 
@@ -13,7 +15,7 @@ public class EmployeeWages {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	
-	private int numOfCompany = 2;
+	private int numOfCompany = 0;
 	private int maxhours_inMonth = 100;
 	private ArrayList< Multiple_Companies> list;
 	
@@ -35,11 +37,11 @@ public class EmployeeWages {
 	}
 	
 	
-	public int computeEmpWage( Multiple_Companies  companyEmpWage) {
+	public int computeEmpWage( Multiple_Companies companyEmpWage) {
 		  int totalWorkinghour = 0;
 		  int days = 0; 
 	      int empHrs = 0;
-	      int dailyEmpWage=0;
+	     // int dailyEmpWage=0;
 	      System.out.println();
 	      System.out.println(companyEmpWage.company);
 	      while(totalWorkinghour <= maxhours_inMonth  && days < companyEmpWage.working_DayperMonth ) {
@@ -48,22 +50,11 @@ public class EmployeeWages {
 			 switch(attendance){
 				
 				case IS_PART_TIME : empHrs = 4;
-				                    dailyEmpWage = companyEmpWage.wagePerhour * empHrs;
-				                    System.out.println("Part Time Employee wages :" + 
-		                                    "\nWages Per Hour :\t"+ companyEmpWage.wagePerhour +
-		                                    "\nWorking Days   :\t"+ companyEmpWage.working_DayperMonth+
-		                                    "\nWorking Hour   :\t"+ empHrs +
-		                                    "\nDaily Wage of an Employee : " + dailyEmpWage);
+				                    calculateWage(companyEmpWage, companyEmpWage.wagePerhour,empHrs );
 				                    break;
 				
 				case IS_FULL_TIME : empHrs = 8;
-				                    dailyEmpWage = companyEmpWage.wagePerhour * empHrs;
-				                    System.out.println("Full Time Employee wages :" + 
-	                                    "\nWages Per Hour :\t"+ companyEmpWage.wagePerhour +
-	                                    "\nWorking Days   :\t"+ companyEmpWage.working_DayperMonth+
-	                                    "\nWorking Hour   :\t"+ empHrs +
-	                                    "\nDaily Wage of an Employee : " + dailyEmpWage);
-				
+				                    calculateWage(companyEmpWage, companyEmpWage.wagePerhour,empHrs );
 		                            break;
 		    	        
 				default: empHrs = 0;
@@ -75,13 +66,25 @@ public class EmployeeWages {
 		return totalWorkinghour * companyEmpWage.wagePerhour ;
 	   }
 	
+	
+	public void calculateWage( Multiple_Companies company,int wagePerHour , int empHrs) {
+		int dailyEmpWage=0;
+		dailyEmpWage = company.wagePerhour * empHrs;
+        System.out.println("Part Time Employee wages :" + 
+                "\nWages Per Hour :\t"+ company.wagePerhour +
+                "\nWorking Days   :\t"+ company.working_DayperMonth+
+                "\nWorking Hour   :\t"+ empHrs +
+                "\nDaily Wage of an Employee : " + dailyEmpWage);
+		
+	}
+
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation ");
 		EmployeeWages empWage = new EmployeeWages();
-		empWage.addCompanyEmpWage("amazon",100,20,8);
-		empWage.addCompanyEmpWage("Flipkart",200,20,8);
+		empWage.addCompanyEmpWage("Reliance",100,20,8);
+		empWage.addCompanyEmpWage("Dmart",200,20,8);
 		empWage.computeEmpWage();
 	}
-	
+ 
 }
