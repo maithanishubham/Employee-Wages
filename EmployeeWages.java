42b22188fbda15dbd780ea6d41cd9e228d14115e
shubham maithani <shubhammaithani1@gmail.com>
2022-08-01 09:55:23 -0700

    Daily wage along with total wage

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,6 +1,5 @@
 package com.employee;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 interface ComputeEmpWage{
 	public void addCompanyEmpWage(String company, int wagePerhour, 
@@ -14,73 +13,75 @@ public class EmployeeWages {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	
-	private int numOfCompany = 0;
-	private int maxhours_inMonth = 10;
-	private ArrayList<Multiple_Companies> list;
+	private int numOfCompany = 2;
+	private int maxhours_inMonth = 100;
+	private ArrayList< Multiple_Companies> list;
 	
 	public  EmployeeWages() {
 		list = new ArrayList<>();
 	}
 	
 	private void addCompanyEmpWage(String company, int wagePerhour, int working_DayperMonth, int fullDayhour) {
-		Multiple_Companies companyEmpWage= new  Multiple_Companies(company,wagePerhour,working_DayperMonth,fullDayhour );
+		 Multiple_Companies companyEmpWage= new   Multiple_Companies(company,wagePerhour,working_DayperMonth,fullDayhour );
 	    list.add(companyEmpWage);
 	}
 	
 	private void computeEmpWage() {
 		for(int i=0;i<list.size();i++) {
-			Multiple_Companies companyEmpWage = list.get(i);
- 			companyEmpWage.settotalEmpWage(this.computeEmpWage(companyEmpWage));
+			 Multiple_Companies companyEmpWage = list.get(i);
+ 			companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
             System.out.println(companyEmpWage);		
 		}
 	}
 	
 	
-	public int computeEmpWage(Multiple_Companies companyEmpWage) {
+	public int computeEmpWage( Multiple_Companies  companyEmpWage) {
 		  int totalWorkinghour = 0;
 		  int days = 0; 
-	      int empHrs = 0;	
+	      int empHrs = 0;
+	      int dailyEmpWage=0;
+	      System.out.println();
+	      System.out.println(companyEmpWage.company);
+	      while(totalWorkinghour <= maxhours_inMonth  && days < companyEmpWage.working_DayperMonth ) {
 			 days = days + 1;
 			 int attendance = (int)(Math.random() *10 )%3;
 			 switch(attendance){
 				
 				case IS_PART_TIME : empHrs = 4;
+				                    dailyEmpWage = companyEmpWage.wagePerhour * empHrs;
+				                    System.out.println("Part Time Employee wages :" + 
+		                                    "\nWages Per Hour :\t"+ companyEmpWage.wagePerhour +
+		                                    "\nWorking Days   :\t"+ companyEmpWage.working_DayperMonth+
+		                                    "\nWorking Hour   :\t"+ empHrs +
+		                                    "\nDaily Wage of an Employee : " + dailyEmpWage);
 				                    break;
 				
 				case IS_FULL_TIME : empHrs = 8;
-		                           break;
+				                    dailyEmpWage = companyEmpWage.wagePerhour * empHrs;
+				                    System.out.println("Full Time Employee wages :" + 
+	                                    "\nWages Per Hour :\t"+ companyEmpWage.wagePerhour +
+	                                    "\nWorking Days   :\t"+ companyEmpWage.working_DayperMonth+
+	                                    "\nWorking Hour   :\t"+ empHrs +
+	                                    "\nDaily Wage of an Employee : " + dailyEmpWage);
+				
+		                            break;
 		    	        
-				default: empHrs = 0;        
+				default: empHrs = 0;
+				         System.out.println("Employee wage is zero");
 		       }
 			 totalWorkinghour = totalWorkinghour + empHrs;
 			 System.out.println("");
-			 System.out.println(companyEmpWage.company);
-			 if(empHrs  == 8) {
-			 System.out.println("Full Time Employee wages :" + 
-			                    "\nWages Per Hour \t"+ empHrs +
-			                    "\nWorking Days   \t"+ companyEmpWage.working_DayperMonth+
-			                    "\nWorking Hour   \t"+ companyEmpWage.fullDayhour);
-	        }else if(empHrs == 4) {
-				 System.out.println("Part Time Employee wages :" + 
-		                    "\nWages Per Hour \t"+ empHrs +
-		                    "\nWorking Days   \t"+ companyEmpWage.working_DayperMonth+
-		                    "\nWorking Hour   \t"+ companyEmpWage.fullDayhour);
-			 }else {
-				 System.out.println("Employee wage is zero");
-			 }
-	      return totalWorkinghour *companyEmpWage.working_DayperMonth;
-	     
-	  }//}
-	
-
+	      }
+		return totalWorkinghour * companyEmpWage.wagePerhour ;
+	   }
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation ");
 		EmployeeWages empWage = new EmployeeWages();
-		empWage.addCompanyEmpWage("Reliance",100,20,8);
-		empWage.addCompanyEmpWage("Dmart",200,20,8);
+		empWage.addCompanyEmpWage("amazon",100,20,8);
+		empWage.addCompanyEmpWage("Flipkart",200,20,8);
 		empWage.computeEmpWage();
 	}
- 
+	
 }
--- Multiple_Companies.java
+++ Multiple_Companies.java
@@ -2,28 +2,34 @@ package com.employee;
 
 public class Multiple_Companies {
 
-	public String company;
-	public int wagePerhour;
-	public int working_DayperMonth;
-	public int fullDayhour;
-	public int totalEmpWage;
-	
-	public Multiple_Companies(String company, int wagePerhour, int working_DayperMonth, int fullDayhour) {
-		this.company = company;
-		this.wagePerhour = wagePerhour;
-		this.working_DayperMonth = working_DayperMonth;
-		this.fullDayhour = fullDayhour;
-	}
-	
-	public void settotalEmpWage(int totalEmpWage) {
-		this.totalEmpWage = totalEmpWage;
-	}
-	
-	public String toString() {
-		return "Total Emp Wage for Company:" + company + "is: " + totalEmpWage;
+	 public String company;
+		public int wagePerhour;
+		public int working_DayperMonth;
+		public int fullDayhour;
+		public int totalEmpWage;
+		public int maxhours_inMonth;
+		
+		public Multiple_Companies(String company, int wagePerhour, int working_DayperMonth, int fullDayhour) {
+			this.company = company;
+			this.wagePerhour = wagePerhour;
+			this.working_DayperMonth = working_DayperMonth;
+			this.fullDayhour = fullDayhour;
+		}
+		
+		
+		public int getTotalEmpWage() {
+			return totalEmpWage;
+		}
+
+		public void setTotalEmpWage(int totalEmpWage) {
+			this.totalEmpWage = totalEmpWage;
+		}
+
+		public String toString() {
+			return "Total Emp Wage for Company " + company + "is: " + totalEmpWage;
+			
+		}
 		
 	}
-	
-}
 
 
